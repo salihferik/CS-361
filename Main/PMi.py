deb77a6c2d511f0f9fcf152029fd590b1e9866b5
salihferik <salihferik@std.sehir.edu.tr>
2015-10-22 09:48:21 +0300

    Initial commit

---
+++ Main/PMi.py
@@ -0,0 +1,2 @@
+def size_list(l):
+    return len(l)
---
+++ Test/TMi.py
@@ -0,0 +1,11 @@
+import unittest
+import Main.PMi
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(Main.PMi.size_list([]), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
